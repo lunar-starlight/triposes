e7198a7f84eb62509c32673df24ee6c4d4be79bd
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2024-11-28 19:57:13 +0100

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,22 @@
+import Lake
+open Lake DSL
+
+package "triposes" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Triposes» where
+  -- add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
