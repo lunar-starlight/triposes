34c3870c84dcf6a8f97808cd3e39ef3aec425b83
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2024-12-05 21:33:42 +0100

    wrote the initial part of the blueprint

--- Triposes/Basic.lean
+++ Triposes/Basic.lean
@@ -1,1 +1,17 @@
-def hello := "world"
+import Mathlib.Order.Heyting.Basic
+import Mathlib.Order.Category.HeytAlg
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.Functor.Basic
+
+universe u v₁ v₂
+
+variable {C : Type u}
+
+section Defn
+
+class Tripos [CCat : CategoryTheory.Category.{v₁} C] : Type (max u v₁ v₂ + 1) where
+  P : CategoryTheory.Functor C HeytAlg.{v₂}
+  --𝔼 : ∀ {A B : CCat.obj} (f : CCat.Hom A B) (a : A)
+  --𝔸 :
+
+end Defn
