67613a85cacb84d20b7e42aff4e9dc0ed09169e9
Andrej Bauer <Andrej.Bauer@andrej.com>
2024-12-14 01:59:03 +0100

    A much improved internal language of fp categories

--- Triposes/FPCatDSL2.lean
+++ Triposes/FPCatDSL2.lean
@@ -4,110 +4,91 @@ open CategoryTheory
 open MonoidalCategory
 
 section ProjDSL
+  open Lean
+
+  /-- A context entry -/
+  declare_syntax_cat fpentry
+  syntax ident ":" term : fpentry
+
+  /-- A context takes the form `x₁ : X₁, …, xₙ : Xₙ` where `xᵢ` are identifiers and `Xᵢ` objects of a category. -/
+  declare_syntax_cat fpcontext
+  syntax fpentry,* : fpcontext
+
+  /-- The syntax of terms -/
+  declare_syntax_cat fpterm
+
+  /-- a variable -/
+  syntax ident : fpterm
+
+  /-- the unique element of the terminal object -/
+  syntax "tt" : fpterm
+
+  /-- ordered pair -/
+  syntax "⟨" fpterm "," fpterm "⟩" : fpterm
+
+  /-- first projection -/
+  syntax "fst" fpterm : fpterm
+
+  /-- second projection -/
+  syntax "snd" fpterm : fpterm
+
+  syntax "(" fpterm ")" : fpterm
+
+  /-- morphism application -/
+  syntax "$(" term ")" fpterm : fpterm
+
+  /-- morphism -/
+  syntax fpcontext "⊢ₑ" fpterm : term
+
+  /-- Convert a context `x₁ : X₁, …, xₙ : Xₙ` to the term `X₁ ⊗ ⋯ ⊗ Xₙ`,
+      making sure that the empty context is the terminal object `X ⊗ 𝟙_ _` and
+      that `x : X` is just `X`, rather than `X ⊗ 𝟙_ 𝒞`.
+  -/
+  partial def prodify : TSyntax `fpcontext → MacroM Term
+  | `(fpcontext| ) => `(𝟙_ _)
+  | `(fpcontext| $_:ident : $A:term) => `($A)
+  | `(fpcontext| $_:ident : $A:term, $Γ:fpentry,*) =>
+    do
+      let Γ ← `(fpcontext| $Γ:fpentry,*)
+      let As ← prodify Γ
+      `($A ⊗ $As)
+  | _ => Macro.throwError "invalid context syntax"
+
+  /-- Given an identifier `x` and a context `Γ`, compute the projection from `Γ` determined by `x`. -/
+  partial def project (x : Name) : TSyntax `fpcontext → MacroM Term
+  | `(fpcontext| ) => Macro.throwError s!"unkown identifier {x}"
+  | `(fpcontext| $y:ident : $A:term) =>
+      -- the only thing that can be projected is `x` by the identity morphism
+      if x = y.getId then `(𝟙 $A) else Macro.throwError s!"unkown identifier {x}"
+  | `(fpcontext| $y:ident : $A:term, $Γ:fpentry,*) =>
+    if x = y.getId then
+      `(ChosenFiniteProducts.fst $A _)
+    else do
+      let Γ ← `(fpcontext| $Γ:fpentry,*)
+      let p ← project x Γ
+      `(ChosenFiniteProducts.snd $A _ ≫ $p)
+  | _ => Macro.throwError "invalid context syntax"
+
+  macro_rules
+  | `($Γ:fpcontext ⊢ₑ $x:ident) => project x.getId Γ
+  | `($Γ:fpcontext ⊢ₑ tt) => do { let A ← prodify Γ ; `(ChosenFiniteProducts.toUnit $A) }
+  | `($Γ:fpcontext ⊢ₑ ⟨ $a:fpterm, $b:fpterm ⟩) => `(ChosenFiniteProducts.lift ($Γ:fpcontext ⊢ₑ $a) ($Γ:fpcontext ⊢ₑ $b))
+  | `($Γ:fpcontext ⊢ₑ fst $a:fpterm) => `(($Γ:fpcontext ⊢ₑ $a) ≫ ChosenFiniteProducts.fst _ _)
+  | `($Γ:fpcontext ⊢ₑ snd $a:fpterm) => `(($Γ:fpcontext ⊢ₑ $a) ≫ ChosenFiniteProducts.snd _ _)
+  | `($Γ:fpcontext ⊢ₑ $($f:term) $a:fpterm) => `(($Γ:fpcontext ⊢ₑ $a) ≫ $f)
+  | `($Γ:fpcontext ⊢ₑ ($a:fpterm)) => `($Γ:fpcontext ⊢ₑ $a)
+end ProjDSL
+
+section Examples
 
   /- We work over a cartesian closed category -/
   universe u v
   variable {𝒞 : Type u} [Category.{v, u} 𝒞] [fp : ChosenFiniteProducts 𝒞]
 
-  /- To simplify the definition of `proj`, we use the terminal object of `𝒞` as the default element of `𝒞`. -/
-  instance : Inhabited 𝒞 where default := 𝟙_ 𝒞
+  open ChosenFiniteProducts
 
-  /-- The product of a list of objects, where we make sure that the product of `[A]` is `A`, rather than `A ⊗ 𝟙_ 𝒞`. -/
-  @[reducible]
-  def listProd : Lean.AssocList Lean.Name 𝒞 → 𝒞
-  | .nil => 𝟙_ 𝒞
-  | .cons _ A .nil => A
-  | .cons _ A As => A ⊗ listProd As
-
-  @[simp]
-  def lookup (As : Lean.AssocList Lean.Name 𝒞) (x : Lean.Name) : 𝒞 := (As.find? x).getD (𝟙_ 𝒞)
-
-  /-- The k-th projection from a product, or the terminal morphism if the index is invalid -/
-  @[reducible]
-  def proj (As : Lean.AssocList Lean.Name 𝒞) (x : Lean.Name) : listProd As ⟶ lookup As x :=
-    match As  with
-    | .nil => fp.toUnit _ -- invalid index
-
-    | .cons x' A .nil =>
-      if h : x' = x then
-        (by
-         simp [Lean.AssocList.find?, h]
-         exact 𝟙 A)
-      else
-        (by
-         simp [Lean.AssocList.find?, not_beq_of_ne h]
-         exact fp.toUnit _)
-
-    | .cons x' A (.cons y B Bs) =>
-      if h : x' = x then
-        (by
-        simp [Lean.AssocList.find?, h]
-        exact fp.fst _ _
-        )
-      else
-        (by
-         simp [Lean.AssocList.find?, not_beq_of_ne h]
-         exact fp.snd _ _ ≫ proj (.cons y B Bs) x)
-
-  /-- Given an association list of objects `As = [x₀ : A₀, …, xₙ : Aₙ]` we can form expressions that denote
-      morphisms `A₀ ⊗ ⋯ ⊗ Aₙ ⟶ B` but are written as if objects are sets. -/
-  inductive Expr (As : Lean.AssocList Lean.Name 𝒞) : 𝒞 → Type _ where
-    /-- Variable `var x` refers to the `x`-th element of `As`. -/
-    | var : ∀ (x : Lean.Name), Expr As (lookup As x)
-    /-- The unique element of the terminal object -/
-    | tt : Expr As (𝟙_ _)
-    /-- Ordered pair -/
-    | pair : ∀ {B C}, Expr As B → Expr As C → Expr As (B ⊗ C)
-    /-- First projection -/
-    | fst : ∀ {B C}, Expr As (B ⊗ C) → Expr As B
-    /-- Second projection -/
-    | snd : ∀ {B C}, Expr As (B ⊗ C) → Expr As C
-    /-- Application of a morphism -/
-    | app : ∀ {B C : 𝒞}, (B ⟶ C) → Expr As B → Expr As C
-
-  declare_syntax_cat fpentry
-  syntax ident ":" term : fpentry
-
-  declare_syntax_cat fpcontext
-  syntax fpentry,* : fpcontext
-
-  syntax "[fpcontext|" fpcontext "]" : term
-
-  macro_rules
-  | `([fpcontext| $[$key:ident : $value:term],* ]) =>
-    let key := key.map (fun x => Lean.quote x.getId)
-    `([$[($key, $value)],*].toAssocList')
-
-  declare_syntax_cat fpterm
-  syntax ident : fpterm
-  syntax "tt" : fpterm
-  syntax "⟨" fpterm "," fpterm "⟩" : fpterm
-  syntax "fst" fpterm : fpterm
-  syntax "snd" fpterm : fpterm
-  syntax "(" fpterm ")" : fpterm
-  syntax "[" term "]" fpterm : fpterm
-
-  syntax fpcontext "⊢ₑ" fpterm : term
-
-  macro_rules
-  | `($Γ:fpcontext ⊢ₑ $x:ident) => `(proj [fpcontext|$Γ] $(Lean.quote x.getId))
-  | `($Γ:fpcontext ⊢ₑ tt) => `(fp.toUnit (listProd [fpcontext|$Γ]))
-  | `($Γ:fpcontext ⊢ₑ ⟨ $a:fpterm, $b:fpterm ⟩) => `(fp.lift ($Γ:fpcontext ⊢ₑ $a) ($Γ:fpcontext ⊢ₑ $b))
-  | `($Γ:fpcontext ⊢ₑ fst $a:fpterm) => `(($Γ:fpcontext ⊢ₑ $a) ≫ fp.fst _ _)
-  | `($Γ:fpcontext ⊢ₑ snd $a:fpterm) => `(($Γ:fpcontext ⊢ₑ $a) ≫ fp.snd _ _)
-  | `($Γ:fpcontext ⊢ₑ [$f:term] $a:fpterm) => `(($Γ:fpcontext ⊢ₑ $a) ≫ $f)
-  | `($Γ:fpcontext ⊢ₑ ($a:fpterm)) => `($Γ:fpcontext ⊢ₑ $a)
-
-  /-- Evaluate an expression to the corresponding morphism -/
-  @[reducible]
-  def Expr.eval (As : Lean.AssocList Lean.Name 𝒞) {B : 𝒞} : Expr As B → (listProd As ⟶ B)
-    | .var k => proj As k
-    | .tt => fp.toUnit _
-    | .pair a b => fp.lift a.eval b.eval
-    | .fst a => a.eval ≫ fp.fst _ _
-    | .snd a => a.eval ≫ fp.snd _ _
-    | .app f a => a.eval ≫ f
-
+  /-- the identity map -/
+  example {X : 𝒞} : X ⟶ X := x : X ⊢ₑ x
 
   /-- the twist morphism -/
   example {X Y : 𝒞} : X ⊗ Y ⟶ Y ⊗ X :=
@@ -117,12 +98,15 @@ section ProjDSL
   example {X : 𝒞} : X ⟶ X ⊗ X :=
   x : X ⊢ₑ ⟨ x, x ⟩
 
+  /-- the first projection is the first projection -/
+  example {X Y : 𝒞} : (p : X ⊗ Y ⊢ₑ fst p) = (p : X ⊗ Y ⊢ₑ $(fp.fst X Y) p) := by simp
+
   /-- identity on the terminal -/
   example : 𝟙_ 𝒞 ⟶ 𝟙_ 𝒞 := ⊢ₑ tt
 
   /-- composition of morphisms -/
   example {X Y Z: 𝒞} (g : Y ⟶ Z) (f : X ⟶ Y): X ⟶ Z :=
-    x : X ⊢ₑ [g] [f] x
+    x : X ⊢ₑ $(g) $(f) x
 
   /-- right associator -/
   def assocRight (X Y Z : 𝒞) : (X ⊗ Y) ⊗ Z ⟶ X ⊗ (Y ⊗ Z) :=
@@ -134,7 +118,7 @@ section ProjDSL
 
   /-- the associators are inverses -/
   example {X Y Z : 𝒞} : assocLeft X Y Z ≫ assocRight X Y Z = 𝟙 _ := by
-   simp [assocLeft, assocRight, proj, List.toAssocList']
+   simp [assocLeft, assocRight]
    aesop_cat
 
-end ProjDSL
+end Examples
